59f28eb074407b5804295cd7f0c410f1198d96b6
Hans Svensson <hans@rymdweb.com>
2025-11-08 18:26:57 +0100

    Realtime Register now shows contacts and domains properly. Cached files routes with data in url are stored in a single place.

--- lib/Samizdat/Controller/Invoice.pm
+++ lib/Samizdat/Controller/Invoice.pm
@@ -310,6 +310,8 @@ sub edit ($self) {
   );
   my $accept = $self->req->headers->{headers}->{accept}->[0];
   if ($accept !~ /json/) {
+    # Override cache path to match template structure and share cache between /invoices/ and /customers/:customerid/invoices/ routes
+    $self->stash(docpath => '/invoice/open/edit/index.html');
     $web->{script} .= $self->render_to_string(template => 'invoice/open/edit/index', format => 'js', toast => $toast);
     return $self->render(web => $web, title => $title, template => 'invoice/open/edit/index', headline => 'invoice/chunks/editlinks');
   } else {
@@ -366,6 +368,8 @@ sub handle ($self) {
   );
   my $accept = $self->req->headers->{headers}->{accept}->[0];
   if ($accept !~ /json/) {
+    # Override cache path to match template structure and share cache between /invoices/ and /customers/:customerid/invoices/ routes
+    $self->stash(docpath => '/invoice/handle/index.html');
     $web->{script} .= $self->render_to_string(template => 'invoice/handle/index', format => 'js', toast => $toast);
     return $self->render(web => $web, title => $title, template => 'invoice/handle/index', headline => 'invoice/chunks/handlelinks');
   } else {
@@ -482,6 +486,8 @@ sub payment ($self) {
   my $accept = $self->req->headers->{headers}->{accept}->[0];
   my $invoiceid = int $self->stash('invoiceid') // 0;
   if ($accept !~ /json/) {
+    # Override cache path to match template structure and share cache between /invoices/ and /customers/:customerid/invoices/ routes
+    $self->stash(docpath => '/invoice/payment/index.html');
     $web->{script} .= $self->render_to_string(template => 'invoice/payment', format => 'js');
     return $self->render(template => 'invoice/payment', layout => 'modal', web => $web, title => $title);
   } else {
@@ -596,6 +602,9 @@ sub remind ($self) {
 
   # GET request - show the reminder form
   if ($accept !~ /json/) {
+    # Override cache path to match template structure and share cache between /invoices/ and /customers/:customerid/invoices/ routes
+    $self->stash(docpath => '/invoice/remind/index.html');
+
     # Get reminder count to show in UI
     my $reminders = $self->app->invoice->reminders($invoiceid);
     my $reminder_count = scalar(@$reminders);
